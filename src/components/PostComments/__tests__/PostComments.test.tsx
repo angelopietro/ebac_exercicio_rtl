6a3c8660e54d5d7d5be93393b9b4bb6369612657
Ângelo Pietro <angelopietror@gmail.com>
2024-01-14 19:22:18 -0300

    Ajuste de arquitetura de diretórios para que o teste fique dentro de __tests__

--- src/components/PostComments/__tests__/PostComments.test.tsx
+++ src/components/PostComments/__tests__/PostComments.test.tsx
@@ -1,6 +1,5 @@
 import { fireEvent, render, screen } from "@testing-library/react";
-import Post from ".";
-import PostComment from ".";
+import PostComment from "..";
 
 describe("Teste para o componente PostComment", () => {
   it("Deve renderizar o componente corretamente", () => {
