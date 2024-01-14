dce201768047b2ec9181d8e450e9063096046a9d
Ângelo Pietro <angelopietror@gmail.com>
2024-01-14 19:16:46 -0300

    Inclusão de teste para inserção de comentários

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,25 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react";
+import Post from ".";
+import PostComment from ".";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+describe("Teste para o componente PostComment", () => {
+  it("Deve renderizar o componente corretamente", () => {
+    render(<PostComment />);
+    expect(screen.getByText("Comentar")).toBeInTheDocument();
+  });
+
+  it("Deve adicionar dois comentários no Post", () => {
+    render(<PostComment />);
+    const textarea = screen.getByTestId("comment-textarea");
+    const button = screen.getByTestId("comment-button");
+    const comentarios = ["Comentário A", "Comentŕio B"];
+
+    comentarios.forEach((comment) => {
+      fireEvent.change(textarea, { target: { value: comment } });
+      fireEvent.click(button);
     });
+
+    const comments = screen.getAllByTestId("comment-data"); 
+    expect(comments).toHaveLength(2);
+  });
 });
